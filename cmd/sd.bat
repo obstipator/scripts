5a341da1576fe12cc323ab3a7ee19fdc9a89fd76
scott <obstipator@gmail.com>
2013-12-30 22:23:18 -0500

    updated sd to be good

--- cmd/sd.bat
+++ cmd/sd.bat
@@ -1,7 +1,1 @@
-@ svn diff | egrep "\+.*?(console\.log|var_dump)" > "%~dp0../temp/svnDumpTemp.txt"
-@ set svndump=
-@ set /p svndump= < "%~dp0../temp/svnDumpTemp.txt"
-@if not defined  svndump (echo "all good") else (
-  start cmd /k "less -p ""(console\.log^|var_dump)"" ""%~dp0../temp/svnDumpTemp.txt"" && echo. > ""%~dp0../temp/svnDumpTemp.txt"" && exit"
-)
-@ svn diff | less
+@svn diff -r PREV | TortoiseUDiff /p /title:"HERE IS THE DIFF"
