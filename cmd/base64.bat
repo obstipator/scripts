80b5a1f4e7fd010f4a2bdd70cfaf4a8c27adf381
scott <obstipator@gmail.com>
2014-05-01 16:58:20 -0400

    base64 improvements

--- cmd/base64.bat
+++ cmd/base64.bat
@@ -1,5 +1,5 @@
 @echo off
-( printf "data:image/@@@;base64," && ( %~dp0../inc/base64.exe -i "%1" -n 0 ) ) | "%~dp0../inc/clip.exe"
+printf "data:image/" && (identify "%1" | gawk "{printf tolower($2)}") && printf ";base64," && ( %~dp0../inc/base64.exe -i "%1" -n 0 ) | "%~dp0../inc/clip.exe"
 echo. 
 echo copied to clipboard
 @echo on
